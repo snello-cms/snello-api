419a292af942e30b4df8a4a90397dd2c0a8e3c24
fiorenzo pizza <fiorenzino@gmail.com>
2019-09-15 11:32:57 +0200

    query mysql in error

--- src/main/java/io/snello/repository/mysql/MysqlConstants.java
+++ src/main/java/io/snello/repository/mysql/MysqlConstants.java
@@ -198,7 +198,7 @@ public class MysqlConstants {
 
 
     public static String creationLinksViewRole = "INSERT IGNORE INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('links_view', 'links view rule', 'metadatas', 'links', 'view');";
-    public static String creationLinksEditRole = "INSERT IGNORE INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('flinks_edit', 'links edit rule', 'metadatas', 'links', 'edit');";
+    public static String creationLinksEditRole = "INSERT IGNORE INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('links_edit', 'links edit rule', 'metadatas', 'links', 'edit');";
 
 
     public static String creationMetadatasViewRole = "INSERT IGNORE INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('metadatas_view', 'metadatas metadatas rule', 'metadatas', 'metadatas', 'view');";
@@ -221,11 +221,11 @@ public class MysqlConstants {
     public static String creationUserViewRole = "INSERT IGNORE INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('users_view', 'users view rule', 'metadatas', 'users', 'view');";
     public static String creationUserEditRole = "INSERT IGNORE INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('users_edit', 'users edit rule', 'metadatas', 'users', 'edit');";
 
-    public static String creationDroppableViewRole = "INSERT INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('droppables_view', 'droppables view rule', 'metadatas', 'droppables', 'view') ON CONFLICT (name) DO NOTHING;";
-    public static String creationDroppableEditRole = "INSERT INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('droppables_edit', 'droppables edit rule', 'metadatas', 'droppables', 'edit') ON CONFLICT (name) DO NOTHING;";
+    public static String creationDroppableViewRole = "INSERT INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('droppables_view', 'droppables view rule', 'metadatas', 'droppables', 'view');";
+    public static String creationDroppableEditRole = "INSERT INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('droppables_edit', 'droppables edit rule', 'metadatas', 'droppables', 'edit');";
 
-    public static String creationDraggableViewRole = "INSERT INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('draggables_view', 'draggables view rule', 'metadatas', 'draggables', 'view') ON CONFLICT (name) DO NOTHING;";
-    public static String creationDraggableEditRole = "INSERT INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('draggables_edit', 'draggables edit rule', 'metadatas', 'draggables', 'edit') ON CONFLICT (name) DO NOTHING;";
+    public static String creationDraggableViewRole = "INSERT INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('draggables_view', 'draggables view rule', 'metadatas', 'draggables', 'view');";
+    public static String creationDraggableEditRole = "INSERT INTO `roles` (`name`, `description`, `object_type`, `object_name`, `action`) VALUES ('draggables_edit', 'draggables edit rule', 'metadatas', 'draggables', 'edit');";
 
 
     public static String creationAdminUserRole = "INSERT IGNORE INTO `userroles` (`username`, `role`) VALUES ('admin', 'admin');";
