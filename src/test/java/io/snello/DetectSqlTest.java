c26696015632ad504d76eba6cd27197100fa693f
alex <aleksandar.mojanchevski@atis.al>
2025-05-09 17:36:48 +0200

    #nan detect sql

---
+++ src/test/java/io/snello/DetectSqlTest.java
@@ -0,0 +1,34 @@
+package io.snello;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+
+public class DetectSqlTest {
+
+    @Test
+    public void notSql1() {
+        String selectSQL = "(SELECT (CASE WHEN (5698=5698) THEN 'ordine ASC' ELSE (SELECT 6357 UNION SELECT 4776) END))";
+        assertFalse(isSqlStatement(selectSQL));
+
+    }
+
+    @Test
+    public void notSql2() {
+        String selectSQL = "(SELECT2 (CASE WHEN (5698=5698) THEN 'ordine ASC' ELSE (SELECT 6357 UNION SELECT 4776) END))";
+        assertFalse(isSqlStatement(selectSQL));
+
+    }
+
+    protected boolean isSqlStatement(String sql) {
+        try {
+            Statement select = (Statement) CCJSqlParserUtil.parse(sql);
+            return true;
+        } catch (Exception e) {
+
+        }
+        return false;
+    }
+
+}
